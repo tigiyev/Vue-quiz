35d0a30cfdc604c5ee9aa5ca478e69eb8f48ae5b
Timur Tigiyev <tigiyev@gmail.com>
2024-01-24 16:22:08 +0400

    ups

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="quiz">
 		<h5>Your result is: {{ totalCorrect }} / {{ questionsAnswered }} </h5>
-		<h2 v-if="totalCorrect >= 2"> You are meme lord!</h2>
+		<h2 v-if="totalCorrect >= questionsAnswered"> You are meme lord!</h2>
 		<h2 v-else> You are healthy human being and don't know memes!</h2>
 	</div>
 </template>
